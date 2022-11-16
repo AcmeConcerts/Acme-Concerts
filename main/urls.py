db3d555a4c96a63e9c5045c6af1bbb2e32819602
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-11-16 22:16:27 +0100

    Detail view finished

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('tickets', views.products, name='tickets'),
