f05cb23b3a1e036442c4729c5c014a390210f135
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-11-16 11:44:39 +0100

    model of tickets

--- main/views.py
+++ main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def products(request):
+    context = {
+        'items': Ticket.objects.all()
+    }
+    return render(request, "products.html", context)
