9e188ba87167bc2571c82f860ed11d87bddd91ef
Pedro Parrilla Bascón <80246477+pedparbas@users.noreply.github.com>
2022-11-22 02:06:10 +0100

    Update settings.py for deployment
    Necesitaba hacer los cambios para el despliegue.

--- AcmeConcerts/settings.py
+++ AcmeConcerts/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sr+w*!4w_*q*2*eu4mshb8s+5unhbtezub=_!#gu(kgf=7ur8*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
