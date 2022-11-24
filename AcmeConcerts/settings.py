9c1888c4cec402c19bd2ad513cfe1b00121356c4
Álvaro Vázquez Ortiz <alvvazort@alum.us.es>
2022-11-24 13:16:46 +0100

    feat: customize view started

--- AcmeConcerts/settings.py
+++ AcmeConcerts/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+    'customize.apps.CustomizeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
